32da6610903646a78539d6185a7a4f95b1047176
root <root@localhost.localdomain>
2017-01-12 08:37:07 -0800

    Adding strict host checking handling

--- demo_ansible/shell_scripts/helloworld.sh
+++ demo_ansible/shell_scripts/helloworld.sh
@@ -1,2 +1,2 @@
 echo "Hellow duniya!"
-ssh -q -i "/home/jjabbar/Desktop/ssh-keys/JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
+ssh -o "StrictHostKeyChecking no" -i "/home/jjabbar/Desktop/ssh-keys/JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
