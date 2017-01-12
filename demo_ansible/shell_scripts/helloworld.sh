c48c923828ba6c1c30937abbf22a6412aef4e683
root <root@localhost.localdomain>
2017-01-12 08:55:09 -0800

    Committing

--- demo_ansible/shell_scripts/helloworld.sh
+++ demo_ansible/shell_scripts/helloworld.sh
@@ -1,6 +1,5 @@
 echo "Hellow duniya!"
 currentPath=`pwd`
 echo "current path [$currentPath]"
-pwd
-chmod 400 JawsDemo.pem
-ssh -o "StrictHostKeyChecking no" -i "JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
+chmod 400 $currentPath/JawsDemo.pem
+ssh -o "StrictHostKeyChecking no" -i "$currentPath/JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
