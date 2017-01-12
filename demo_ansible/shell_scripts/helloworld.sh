22172a821198217d9a251c438c775c4e9ef64bde
root <root@localhost.localdomain>
2017-01-12 08:48:55 -0800

    Fid

--- demo_ansible/shell_scripts/helloworld.sh
+++ demo_ansible/shell_scripts/helloworld.sh
@@ -1,3 +1,3 @@
 echo "Hellow duniya!"
-chmod 400 ./JawsDemo.pem
-ssh -o "StrictHostKeyChecking no" -i "./JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
+chmod 400 JawsDemo.pem
+ssh -o "StrictHostKeyChecking no" -i "JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
