b332e38ce73d6f940a80a1bd929f28d11cb7b93f
root <root@localhost.localdomain>
2017-01-12 08:56:01 -0800

    Fixing ssh and chmod problem

--- demo_ansible/shell_scripts/helloworld.sh
+++ demo_ansible/shell_scripts/helloworld.sh
@@ -1,5 +1,5 @@
 echo "Hellow duniya!"
 currentPath=`pwd`
 echo "current path [$currentPath]"
-chmod 400 $currentPath/JawsDemo.pem
-ssh -o "StrictHostKeyChecking no" -i "$currentPath/JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
+chmod 400 $currentPath/shell_scripts/JawsDemo.pem
+ssh -o "StrictHostKeyChecking no" -i "$currentPath/shell_scripts/JawsDemo.pem" ec2-user@ec2-52-201-234-249.compute-1.amazonaws.com "cd /home/ec2-user/; touch helloWorld"
